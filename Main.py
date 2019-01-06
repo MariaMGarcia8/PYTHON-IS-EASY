8f73346b0ed6196d0ccb6141322a51ca783b498c
MariaMGarcia8 <46412579+MariaMGarcia8@users.noreply.github.com>
2019-01-05 23:38:45 -0600

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,23 @@
+Artist = "Jewel"
+SongName = "Hands"
+Genra = "Pop"
+YearReleased = "1998"
+BillBoard_Chart = "6"
+DurationINseconds = 227
+#Spirit was Jewels second album release.
+Album = "Spirit"
+RecordLabel = "Atlantic"
+Songwriter_One = "Jewel Kilcher"
+#Patrick Leonard was also the producer.
+Songwriter_Two = "Patrick Leonard"
+
+print(Artist)
+print(SongName)
+print(Genra)
+print(YearReleased)
+print(BillBoard_Chart)
+print(DurationINseconds)
+print(Album)
+print(RecordLabel)
+print(Songwriter_One)
+print(Songwriter_Two)
