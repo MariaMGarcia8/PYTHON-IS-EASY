5912bf3fe7bdce4071105adfd267f197ab6b68ba
MariaMGarcia8 <46412579+MariaMGarcia8@users.noreply.github.com>
2019-01-06 14:47:56 -0600

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,18 @@
+def Artist(name_of_artist):
+    name_of_artist = "Jewel"
+    print(name_of_artist)
+    return name_of_artist
+
+def SongName(name_of_song):
+    name_of_song = "Hands"
+    print(name_of_song)
+    return name_of_song
+
+def Album(name_of_Album):
+    name_of_Album = "Spirit"
+    print(name_of_Album)
+    return name_of_Album
+
+Artist(0)
+SongName(0)
+Album(0)
