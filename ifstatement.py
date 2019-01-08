6a8851188d1717a1a2e1dde5dce9b2e04b9efb65
MariaMGarcia8 <46412579+MariaMGarcia8@users.noreply.github.com>
2019-01-07 22:47:22 -0600

    Add files via upload

---
+++ ifstatement.py
@@ -0,0 +1,14 @@
+
+
+a = 5
+b = a + 5
+c = 10
+equality = True
+def Letters(ABC):
+    if a == b or b >= c:
+        equality = True
+    elif a == b and a == c:
+        equality = False
+    print(equality)
+
+Letters(0)
